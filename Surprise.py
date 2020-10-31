c41dfa776a2b37fac943dfe461a84d1a98d636d9
alifsyr <alifsyr@gmail.com>
2020-11-01 00:30:59 +0700

    add new features

--- Surprise.py
+++ Surprise.py
@@ -1,5 +1,4 @@
-
-
+import urllib.request
 def verifikasi(nama):
     if(nama == "Fara" or nama == "Fathiya" or nama == "Fathiya Shafira" or nama == "Shafira" or nama == "fara" or nama == "fathiya" or nama == "fathiya shafira" or nama == "shafira"):
         print("Hai,",nama,". kamu cantik banget hari ini 💕.")
@@ -21,24 +20,35 @@ def pertanyaan(jawaban):
         else:
             exit()
 
-def jadian(x):
-    if (x == "30/08/2019"):
+def jadian(tanggal):
+    if (tanggal == "30/08/2019"):
         kejutan(input("Kamu mau liat surprisenya ? (iya/tidak) :"))
     else:
         print("YAHH, kamu salah tanggal. aladdin bisa marah nih kalau tau")
         jadian((input("Coba inget-inget lagi, kapan kalian jadian ? (contoh : 01/01/2001) : ")))
 
-def kejutan(x):
-    if (x == "iya" or x == "Iya"):
-        print("buka link ini. http://bit.ly/UntukFara")
+def kejutan(answers):
+    if (answers == "iya" or answers == "Iya"):
+        url = ("https://i.ibb.co/27jFQJB/FARA.png")
+        file_path = 'assets/kejutan/'
+        file_name = 'Untuk pacarnya Aladdin'
+        dl_img(url,file_path,file_name)
         Exit (input("Apakah kamu mau keluar ? (y/n) : "))
     else:
         kepastian = input("Sekali lagi aku tanya, Kamu mau tau surprisenya ? (iya/tidak) : ")
         if (kepastian =="iya" or kepastian == "Iya" ):
-            print("buka link ini. http://bit.ly/UntukFara")
+            url = ("https://i.ibb.co/27jFQJB/FARA.png")
+            file_path = 'assets/kejutan/'
+            file_name = 'Untuk pacarnya Aladdin'
+            dl_img(url,file_path,file_name)
             Exit (input("Apakah kamu mau keluar ? (y/n) : "))
         else:
             exit()
+
+def dl_img(url,file_path,file_name):
+    full_path = file_path + file_name + '.png'
+    urllib.request.urlretrieve(url,full_path)
+    print("Sekarang kamu klik folder assets, lalu kamu klik folder kejutan 👍")
 
 def Exit(x):
     if (x == "y" or x == "N"):
@@ -47,7 +57,7 @@ def Exit(x):
         verifikasi(input("Halo, dengan siapa ni ??? : ").capitalize())
     else:
         exit()
-        
+
 print("Halo👋 aku Jarvis")
 verifikasi(input("Kalau kamu siapa ni ??? : ").capitalize())
 
